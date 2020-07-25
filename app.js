52bf0bbb2c6460d5fff797a81e01f595bfd79cb8
Thando Mini <tzone85@gmail.com>
2020-07-25 07:34:14 +0200

    added some routing to the project, handled the response and wrote to an external file

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    //process.exit();
+    
+    const url = req.url;
+    const method = req.method;
+    if (url === '/'){
+        res.write('<html>');
+        res.write('<head><title>MEnter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+
+        return res.end();
+    }
+
+    if (url === '/message' && method === 'POST') {
+        fs.writeFileSync('msg.txt', 'dummy');
+        // res.writeHead(302, {}) one way of doing the below
+
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
 
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
