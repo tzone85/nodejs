645fe962d14bd3b4b62372c65964726cbb21fb88
Thando Mini <tzone85@gmail.com>
2020-07-25 06:38:36 +0200

    some basic way of sending data back from the server to the client

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
+    //process.exit();
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hellow from Node.js Server</h1></body>');
+    res.write('</html>');
+
+    res.end();
 });
 
 server.listen(3000);
