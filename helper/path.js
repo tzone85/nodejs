d33958c936995dfca769988f2892318b07a7f430
Thando Mini <tzone85@gmail.com>
2020-07-29 07:45:22 +0200

    made the path to the app causing the app to run standard from all routs and exported it for consumption in the different routes. Much cleaner as there's no overhead and there's no assumptions as to what OS the user might be using

---
+++ helper/path.js
@@ -0,0 +1,4 @@
+const path = require('path');
+
+// for path to the file that ensures our app is running
+module.exports = path.dirname(process.mainModule.filename);
--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const express = require('express');
 
+const rootDir = require('../helper/path');
+
 const router = express.Router();
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
     console.log("in another the middleware");
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
     // res.send("<form action='/admin/add-product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button> </form>")
 });
 
