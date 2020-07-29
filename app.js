7ef458f1009ccfe68a27b6ccea28264c11cb3b74
Thando Mini <tzone85@gmail.com>
2020-07-29 07:30:46 +0200

    added views dir, some pages there. Modified the routing such that it serves the new pges

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+// to send a 404 for unregistered routes
+app.use((req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', 'page-not-found.html'));
+    // res.status(404).send('<h1>Page not found</h1>');
+})
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 
 const router = express.Router();
@@ -5,7 +6,8 @@ const router = express.Router();
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
     console.log("in another the middleware");
-    res.send("<form action='/admin/add-product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button> </form>")
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+    // res.send("<form action='/admin/add-product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button> </form>")
 });
 
 // /admin/add-product => POST
