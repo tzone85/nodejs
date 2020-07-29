5163aa5d604bd9f31f81a3b28b1bc4ae74260c7f
Thando Mini <tzone85@gmail.com>
2020-07-29 06:42:28 +0200

    streamlined our middleware routing and redirection for the admin

--- routes/shop.js
+++ routes/shop.js
@@ -1,6 +1,10 @@
 const express = require('express');
 
-app.use('/', (req, res, next) => {
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
     console.log("in another the middleware");
     res.send("<h1>Hi From Express </h1>");
 });
+
+module.exports = router;
