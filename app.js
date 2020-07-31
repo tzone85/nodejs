940f6cffc2aa2fe7a7bad3eafb290234e1b87e47
Thando Mini <tzone85@gmail.com>
2020-07-31 04:22:19 +0200

    added some ejs for templating and separated the logic accordingly. Experimented with pug and mastache

--- app.js
+++ app.js
@@ -3,21 +3,22 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/admin', adminRoutes);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
-// to send a 404 for unregistered routes
-app.use((req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', 'page-not-found.html'));
-    // res.status(404).send('<h1>Page not found</h1>');
-})
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+});
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,22 @@
 const path = require('path');
+
 const express = require('express');
 
-const rootDir = require('../helper/path');
+const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    console.log("in another the middleware");
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
